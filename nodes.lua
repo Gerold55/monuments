e529f797fa5b4c9cfe96fc388f5956a8b4648f2c
Gerold55 <marleybeare56@gmail.com>
2018-02-07 17:49:34 -0500

    Add files via upload

--- nodes.lua
+++ nodes.lua
@@ -20,3 +20,26 @@ minetest.register_node("monuments:10commandments", {
 	}
 })
 
+minetest.register_node("monuments:cup", {
+	description = "Golden Cup",
+	tiles = {
+		"default_gold_block.png",
+		"default_gold_block.png",
+		"default_gold_block.png",
+		"default_gold_block.png",
+		"default_gold_block.png",
+		"default_gold_block.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, -0.5, -0.125, 0.125, -0.375, 0.125}, 
+			{-0.0625, -0.375, -0.0625, 0.0625, 0, 0.0625},
+			{-0.1875, -0.1875, -0.1875, 0.1875, 0.3125, 0.1875},
+		}
+	}
+})
